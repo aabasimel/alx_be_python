fc38ad4c3efbe1ea882837d52b88f8d0ca8905d2
Ahmed <abasimelahmed@gmail.com>
2025-09-08 12:19:26 +0200

    Added basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,5 @@
+number1=10
+number2=5
+print(f"The sum of {number1} and {number2} is {number1 +number2}")
+print(f"The difference of {number1} and {number2} is {number1 - number2}")
+print(f"The product of {number1} and {number2} is {number1 * number2}")
